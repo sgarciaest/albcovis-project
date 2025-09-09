bc1ed2099256e932c3b4e50fcf311783b0df4f37
sgarciaest <sergio.garcia11@alumni.esade.edu>
2025-09-09 18:52:53 +0200

    Improved return of detect_faces with more metrics

--- src/albcovis/services/face_detection.py
+++ src/albcovis/services/face_detection.py
@@ -1,12 +1,42 @@
 from deepface import DeepFace
+from PIL import Image
+from typing import List, Dict
 
-def dectect_faces(path: str):
+def detect_faces(path: str) -> Dict:
     # Ensure the path is a string and not Path object
     path = str(path)
+
+    img = Image.open(path)
+    w, h = img.size
+    img_area = w * h
+    
     faces = DeepFace.extract_faces(path, detector_backend="yolov11n", enforce_detection=False)
+    
     # Don't return the array of pixels, only bboxes and confidence score
-    faces_no_image = [
-        {"facial_area": d["facial_area"], "confidence": d["confidence"]}
+
+    keys_to_remove = ["left_eye", "right_eye"]
+    faces = [
+        {
+            "bbox": list({k: v for k, v in d["facial_area"].items() if k not in keys_to_remove}.values()),
+            "segmentation": [[d["facial_area"]["x"], d["facial_area"]["y"], d["facial_area"]["x"] + d["facial_area"]["w"], d["facial_area"]["y"], d["facial_area"]["x"] + d["facial_area"]["w"], d["facial_area"]["y"] + d["facial_area"]["h"], d["facial_area"]["x"], d["facial_area"]["y"] + d["facial_area"]["h"]]]
+            "area": d["facial_area"]["w"] * d["facial_area"]["h"],
+            "relative_size": (d["facial_area"]["w"] * d["facial_area"]["h"]) / img_area
+            "confidence": d["confidence"]
+        }
         for d in faces
     ]
-    return faces_no_image
+
+    aggregated_faces_stats = {
+        "n_faces": len(faces)
+        "mean_area": mean([f["area"] for f in faces])
+        "largest_face": XXXXX,
+        "highest_confidence_face" XXXXXX,
+        "average_relative_size": sum([f["area"] for f in faces]) / img_area
+        "face_density": XXXXXXXXX
+    }
+
+
+
+    n_faces = len(faces)
+
+    return faces
